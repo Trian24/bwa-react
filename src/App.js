3db409a2408ad8f2752d9afd2792bda1292bf153
Minka <renalditriananda@gmail.com>
2021-02-22 20:00:46 +0700

    Add Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import './App.css';
 import 'assets/scss/style.scss';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import landing from 'pages/landing';
+
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Route path="/" component={landing}>
+
+        </Route>
+      </Router>
     </div>
   );
 }
--- src/pages/landing.js
+++ src/pages/landing.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Header from 'parts/header';
 
 class landing extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <>
+        <Header {...this.props}></Header>
+      </>
     );
   }
 }
